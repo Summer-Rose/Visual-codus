cbf57da329308c7f161072e761c836fa6534dc2e
Summer <sbrochtrup@gmail.com>
2015-09-09 10:33:51 -0700

    Initial commit: initial file structure

---
+++ src/test/java/AppTest.java
@@ -0,0 +1,25 @@
+import org.fluentlenium.adapter.FluentTest;
+import java.util.ArrayList;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import static org.fluentlenium.core.filter.FilterConstructor.*;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AppTest extends FluentTest {
+  public WebDriver webDriver = new HtmlUnitDriver();
+  public WebDriver getDefaultDriver() {
+    return webDriver;
+  }
+
+  @Rule
+  public DatabaseRule database = new DatabaseRule();
+
+  @ClassRule
+  public static ServerRule server = new ServerRule();
+
+
+}
