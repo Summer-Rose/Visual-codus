864117dd110caee701256741b2ad17340e19055d
Summer <student@epicodus.com>
2015-09-10 12:01:46 -0700

    Condense getSalaryRange method

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -104,6 +104,14 @@ public class Student {
       Integer students010 = 0;
       Integer students1020 = 0;
       Integer students2030 = 0;
+      Integer students3040 = 0;
+      Integer students4050 = 0;
+      Integer students5060 = 0;
+      Integer students6070 = 0;
+      Integer students7080 = 0;
+      Integer students8090 = 0;
+      Integer students90plus = 0;
+
       for(Integer salary : salaries) {
         if (salary < 10000) {
           students010++;
@@ -111,17 +119,33 @@ public class Student {
           students1020++;
         } else if (salary >= 20000 && salary < 30000) {
           students2030++;
+        } else if (salary >= 30000 && salary < 40000) {
+          students3040++;
+        } else if (salary >= 40000 && salary < 50000) {
+          students4050++;
+        } else if (salary >= 50000 && salary < 60000) {
+          students5060++;
+        } else if (salary >= 60000 && salary < 70000) {
+          students6070++;
+        } else if (salary >= 70000 && salary < 80000) {
+          students7080++;
+        } else if (salary >= 80000 && salary < 90000) {
+          students8090++;
+        } else {
+          students90plus++;
         }
       }
-      String div010 = String.format("<div style=\"height: 20px; width: %d%%; background-color: red\">", students010);
-      String div1020 = String.format("<div style=\"height: 20px; width: %d%%; background-color: red\">", students1020);
-      String div2030 = String.format("<div style=\"height: 20px; width: %d%%; background-color: red\">", students2030);
-      salaryRanges.put("salary010", div010);
-      salaryRanges.put("salary1020", div1020);
-      salaryRanges.put("salary2030", div2030);
-      System.out.println("TEST: " + salaryRanges.get("salary010"));
-      System.out.println("TEST: " + salaryRanges.get("salary1020"));
-      System.out.println("TEST: " + salaryRanges.get("salary2030"));
+
+      salaryRanges.put("salary010", String div010 = String.format("<div style=\"height: 20px; width: %d%%; background-color: red\"><p>$0-10,000</p></div>", students010 * 100 / Student.all().size()));
+      salaryRanges.put("salary1020", String div1020 = String.format("<div style=\"height: 20px; width: %d%%; background-color: red\"><p>$10- 20,000</p></div>", students1020 * 100 / Student.all().size()));
+      salaryRanges.put("salary2030", String div2030 = String.format("<div style=\"height: 20px; width: %d%%; background-color: red\"><p>$20-30,000</p></div>", students2030 * 100 / Student.all().size()));
+      salaryRanges.put("salary3040", String div3040 = String.format("<div style=\"height: 20px; width: %d%%; background-color: red\"><p>$30-40,000</p></div>", students3040 * 100 / Student.all().size()));
+      salaryRanges.put("salary4050", String div4050 = String.format("<div style=\"height: 20px; width: %d%%; background-color: red\"><p>$40-50,000</p></div>", students4050 * 100 / Student.all().size()));
+      salaryRanges.put("salary5060", String div5060 = String.format("<div style=\"height: 20px; width: %d%%; background-color: red\"><p>$50-60,000</p></div>", students5060 * 100 / Student.all().size()));
+      salaryRanges.put("salary6070", String div6070 = String.format("<div style=\"height: 20px; width: %d%%; background-color: red\"><p>$60-70,000</p></div>", students6070 * 100 / Student.all().size()));
+      salaryRanges.put("salary7080", String div7080 = String.format("<div style=\"height: 20px; width: %d%%; background-color: red\"><p>$70-80,000</p></div>", students7080 * 100 / Student.all().size()));
+      salaryRanges.put("salary8090", String div8090 = String.format("<div style=\"height: 20px; width: %d%%; background-color: red\"><p>$80-90,000</p></div>", students8090 * 100 / Student.all().size()));
+      salaryRanges.put("salary90plus", String div90plus = String.format("<div style=\"height: 20px; width: %d%%; background-color: red\"><p>$90,000+</p></div>", students90plus * 100 / Student.all().size()));
       return salaryRanges;
     }
   }
